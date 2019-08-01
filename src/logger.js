94e8d764545e831378fd66fe6fda503bbe22b03c
leegeunhyeok <lghlove0509@naver.com>
2019-08-01 13:33:35 +0900

    UPDATE: Logger logic

--- src/logger.js
+++ src/logger.js
@@ -2,23 +2,49 @@ const config = require('../config')
 
 const fs = require('fs')
 
-const writer = (...args) => {
+const Logger = {}
+Logger._timestamp = () => {
+
+  function pad (t, s, w = '0') {
+    t = t.toString()
+    while (t.length < s) t = w + t
+    return t
+  }
+
+  const _ = new Date()
+  const yyyy = pad(_.getFullYear(), 4)
+  const MM = pad(_.getMonth() + 1, 2)
+  const dd = pad(_.getDate(), 2)
+  const hh = pad(_.getHours(), 2)
+  const mm = pad(_.getMinutes(), 2)
+  const ss = pad(_.getSeconds(), 2)
+  const ms = pad(_.getMilliseconds(), 3)
+
+  return `${yyyy}.${MM}.${dd} ${hh}:${mm}:${ss}.${ms} `
+}
+
+Logger._msg = function (...args) {
+  return this._timestamp() + args.join(' ')
+}
+
+Logger._writer = msg => {
   fs.writeFile(
     config.log,
-    args.join(' ') + '\n', {
+    msg + '\n', {
       flag: 'a'
     }, _err => void 0)
 }
 
-const Logger = {}
-Logger.info = (...args) => {
-  console.log('INFO:', ...args)
-  writer('INFO:', ...args)
+Logger.info = function (...args) {
+  const msg = this._msg('INFO -', ...args)
+  console.log(msg)
+  this._writer(msg)
 }
 
-Logger.error = (...args) => {
-  console.error('ERROR: ', ...args)
-  writer('ERROR: ',...args)
+Logger.error = function (...args) {
+  const msg = this._msg('ERROR -', ...args)
+  console.error(msg)
+  this._writer(msg)
 }
 
 module.exports = Logger
