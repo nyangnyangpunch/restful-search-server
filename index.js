9220e1cb7f625b1474eb94377cde06f40c226f45
leegeunhyeok <lghlove0509@naver.com>
2019-08-01 15:33:52 +0900

    UPDATE: Add comment

--- index.js
+++ index.js
@@ -5,21 +5,30 @@ const search = require('./src/search')
 const express = require('express')
 const app = express()
 
+// Uncaught exception handler 
 process.on('uncaughtException', logger.error)
 
+// API Route and handler
 app.get(config.endPoint, async (req, res) => {
   const keyword = req.query.query
   try {
+    // Keyword search
     logger.info(`Search keyword: ${keyword}`)
+
+    // Set response header
     res.setHeader('content-type', ['text/json', 'charset=utf-8'])
     res.write(await search(keyword))
     res.end()
   } catch (e) {
+    // Error
     logger.error(e)
-    res.json(null)
+    res.setHeader('content-type', ['text/json', 'charset=utf-8'])
+    res.write(null)
+    res.end()
   }
 })
 
+// LISTEN (Server start)
 app.listen(config.port, () => {
   logger.info('Server started')
 })
--- src/logger.js
+++ src/logger.js
@@ -2,15 +2,22 @@ const config = require('../config')
 
 const fs = require('fs')
 
+// Logger object
 const Logger = {}
-Logger._timestamp = () => {
 
+/**
+ * @description Generate timestamp string
+ * @return { String } Timestamp string
+ */
+Logger._timestamp = () => {
+  // Add padding
   function pad (t, s, w = '0') {
     t = t.toString()
     while (t.length < s) t = w + t
     return t
   }
 
+  // Current date
   const _ = new Date()
   const yyyy = pad(_.getFullYear(), 4)
   const MM = pad(_.getMonth() + 1, 2)
@@ -23,10 +30,21 @@ Logger._timestamp = () => {
   return `${yyyy}.${MM}.${dd} ${hh}:${mm}:${ss}.${ms} `
 }
 
+
+/**
+ * @description Spread all args and join with whitespace
+ * @param { any } args Arguments
+ * @return { String }
+ */
 Logger._msg = function (...args) {
   return this._timestamp() + args.join(' ')
 }
 
+
+/**
+ * @description Write message to file (log file)
+ * @param { String } msg Log data
+ */
 Logger._writer = msg => {
   fs.writeFile(
     config.log,
@@ -35,12 +53,22 @@ Logger._writer = msg => {
     }, _err => void 0)
 }
 
+
+/**
+ * @description INFO Level log
+ * @param args
+ */
 Logger.info = function (...args) {
   const msg = this._msg('INFO -', ...args)
   console.log(msg)
   this._writer(msg)
 }
 
+
+/**
+ * @description ERROR Level log
+ * @param args
+ */
 Logger.error = function (...args) {
   const msg = this._msg('ERROR -', ...args)
   console.error(msg)
